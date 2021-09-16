fed357f184d57a6d29b39c2ee9da2a30930fe1dc
DAVI ALVES DOS SANTOS <davi.santos@uvvnet.com.br>
2021-09-16 15:08:32 -0300

    Implementação inicial

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+//Shift R = Reload completo da aplicação;
+
+void main() {
+  runApp(MeuHello());
+}
+
+class MeuHello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.blue,
+          child: ListView(
+            children: [
+              SizedBox(
+                  height: 300, child: Image.asset("assets/images/screen.png")),
+              Expanded(
+                child: Container(
+                  height: 50,
+                  color: Colors.white,
+                  child: Center(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "E-mail",
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      keyboardType: TextInputType.name,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Expanded(
+                child: Container(
+                  height: 50,
+                  color: Colors.white,
+                  child: Center(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Senha",
+                        prefixIcon: Icon(Icons.security),
+                      ),
+                      keyboardType: TextInputType.name,
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color(0xFF539FCB),
+      ),
+    );
+  }
+}
