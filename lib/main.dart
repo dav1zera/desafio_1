d4d405b2820225220ecc4db795eff9101659f13a
DAVI ALVES DOS SANTOS <davi.santos@uvvnet.com.br>
2021-09-16 18:06:17 -0300

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,64 @@ class MeuHello extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          color: Colors.blue,
-          child: ListView(
-            children: [
-              SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/screen.png",
-                  )),
-              Container(
-                height: 50,
-                color: Colors.white,
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "E-mail",
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    keyboardType: TextInputType.name,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 50,
-                color: Colors.white,
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Senha",
-                      prefixIcon: Icon(Icons.security),
-                    ),
-                    keyboardType: TextInputType.name,
-                    obscureText: true,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
         backgroundColor: Color(0xFF539FCB),
+        body: ListView(
+          children: [
+            SizedBox(
+              height: 200,
+              child: Image.asset(
+                "assets/images/screen.png",
+              ),
+            ),
+            Container(
+              height: 50,
+              color: Colors.white,
+              child: Center(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "E-mail",
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              color: Colors.white,
+              child: Center(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                    prefixIcon: Icon(Icons.security),
+                  ),
+                  keyboardType: TextInputType.name,
+                  obscureText: true,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Oi");
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                onPrimary: Color(0xFF5386CB),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
